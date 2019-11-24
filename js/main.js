3e0d3d5e69c8fafc70f3b2f6cd5ab5ba8707e4ec
pipisasa <emir0725mk@gmail.com>
2019-11-25 01:06:53 +0600

    Dohuya chego

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
+
+//______________ Якоря ________ Anchor's __________//
 $(document).ready(function(){
+    //Анимация скролла
     $("#anchorBar").on("click","a", (event)=>{
         //отменяем стандартную обработку нажатия по ссылке
         event.preventDefault();
@@ -11,4 +14,35 @@ $(document).ready(function(){
         //анимируем переход на расстояние - top за 1500 мс
         $('body,html').animate({scrollTop: top}, 1500);
     });
+
+    //Гамбургер
+    $('#Header-hamburger').on('click',(e)=>{
+        $('#mb-menu').toggleClass("hidden");
+        $('body').toggleClass("no-scroll");
+    });
+    $('.Header-nav-item--folder-rooms').on('click',(e)=>{
+        e.stopPropagation()
+        $('#mb-menu-rooms').toggleClass('hidden');
+    });
+    $('.Header-nav-item--folder-adventures').on('click',(e)=>{
+        e.stopPropagation()
+        $('#mb-menu-adventures').toggleClass('hidden');
+    });
+    $('.mb-Header-nav-folder-bck').on('click',(e)=>{
+        e.stopPropagation()
+        $('#mb-menu-adventures').addClass('hidden');
+        $('#mb-menu-rooms').addClass('hidden');
+
+    });
+});
+
+
+//____________   КАРТА 2гис   _______   MAP 2GIS    _______//
+var map;
+DG.then(function () {
+    map = DG.map('map', {
+        center: [42.877737, 74.59211],
+        zoom: 15
+    });
+    DG.marker([42.877737, 74.59221]).addTo(map).bindPopup('Maker\'s bootcamp!');
 });
