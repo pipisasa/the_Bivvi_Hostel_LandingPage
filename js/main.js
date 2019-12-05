c6e147b48da854eb19f5b5504c8329aed15a9eb5
pipisasa <emir0725mk@gmail.com>
2019-12-05 21:29:45 +0600

    reserve completedgit branch!

--- js/main.js
+++ js/main.js
@@ -35,14 +35,3 @@ $(document).ready(function(){
 
     });
 });
-
-
-//____________   КАРТА 2гис   _______   MAP 2GIS    _______//
-var map;
-DG.then(function () {
-    map = DG.map('map', {
-        center: [42.877737, 74.59211],
-        zoom: 15
-    });
-    DG.marker([42.877737, 74.59221]).addTo(map).bindPopup('Maker\'s bootcamp!');
-});
